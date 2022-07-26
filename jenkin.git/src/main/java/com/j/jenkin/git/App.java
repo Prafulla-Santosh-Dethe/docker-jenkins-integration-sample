94d1106469902f12fb3489a9b6a76206a1e971a1
Prafulla_Dethe <dethe27999@gmail.com>
2022-07-26 16:53:04 +0530

    docker added

---
+++ jenkin.git/src/main/java/com/j/jenkin/git/App.java
@@ -0,0 +1,13 @@
+package com.j.jenkin.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
